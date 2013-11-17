9a83ba6e819fe277f274d4dcd6fb7fea3cfec4bf
William Cannon <william.cannon@gmail.com>
2013-11-17 17:14:17 -0600

    initial commit

---
+++ election_votes_generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''This program generates a CSV file, filling it with random votes'''
+import csv
+from docopt import docopt
+
+
+if __name__ == "__main__":
+  arguments = docopt(__doc__)
